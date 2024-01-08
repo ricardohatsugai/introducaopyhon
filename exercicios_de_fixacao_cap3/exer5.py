fbb3e3aea02a84c77306b6fdc5bde95f4706628c
Ricardo Hatsugai <rhatsugai@gmail.com>
2024-01-08 17:12:09 -0300

    Exercício 5 do cap3 - Programação com desvios

---
+++ exercicios_de_fixacao_cap3/exer5.py
@@ -0,0 +1,29 @@
+''' Projeto.....: Exercício 5
+    Autor.......: Ricardo Hatsugai
+    Data........: 08/01/2024
+    Versão......: 1.0
+    Descrição...: Efetuar a leitura de quatro valores numéricos inteiros representados pelas variáveis A, B, C e D. 
+    Apresentar apenas os valores que sejam divisíveis por 2 e 3.'''
+
+a = int(input("Informe o valor da variável A: "))
+b = int(input("Informe o valor da variável B: "))
+c = int(input("Informe o valor da variável C: "))
+d = int(input("Informe o valor da variável D: "))
+
+
+if (a % 2 == 0):
+    print("O valor de A é divisível por 2.")
+if (a % 3 == 0):
+    print("O valor de A é divisível por 3.")
+if (b % 2 == 0):
+    print("O valor de B é divisivel por 2.")
+if (b % 3 == 0):
+    print("O valor de B é divisível por 3.")
+if (c % 2 == 0):
+    print("O valor de C é divisível por 2.")
+if (c % 3 == 0):
+    print("O valor de C é divisível por 3.")
+if (d % 2 == 0):
+    print("O valor de D é divisível por 2.")
+if (d % 3 == 0):
+    print("O valor de D é divisível por 3.")
